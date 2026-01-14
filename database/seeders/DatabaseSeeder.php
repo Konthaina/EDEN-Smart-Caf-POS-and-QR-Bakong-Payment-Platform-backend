0c30fcb5472e7dc328c29717e141f6f73b0eaca1
Konthaina <thainakon87@gmail.com>
2026-01-14 10:29:10 +0700

    update ENV

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,5 +17,18 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
         ]);
+
+        $adminRole = Role::where('name', 'Admin')->first();
+
+        if ($adminRole) {
+            User::firstOrCreate(
+                ['email' => env('ADMIN_EMAIL', 'admin@example.com')],
+                [
+                    'name' => env('ADMIN_NAME', 'Admin'),
+                    'password' => env('ADMIN_PASSWORD', 'password'),
+                    'role_id' => $adminRole->id,
+                ]
+            );
+        }
     }
 }
